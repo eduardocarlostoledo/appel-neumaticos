338efae4ce69eb06959d1af0ceae95b1003b332c
eduardocarlostoledo <eduardocarlostoledo@gmail.com>
2025-04-22 05:39:28 -0300

    ajustes de google console

--- src/components/maxSeo/VehicleTypeSection.jsx
+++ src/components/maxSeo/VehicleTypeSection.jsx
@@ -1,15 +1,28 @@
 import "../../styles/VehicleTypeSection.css"; // Asegúrate de que la ruta sea correcta
 
-
 export const VehicleTypeSection = () => {
   const vehicleTypes = [
     {
       name: "Autos",
       image: "/autos.jpg",
       alt: "Neumáticos y cubiertas para auto, moto y pickup en Encarnación",
-      description: "Desde city cars hasta berlinas de lujo"
+      description: "Desde city cars hasta berlinas de lujo",
+      price: "50000",
     },
-    // Agregar camionetas, motos, etc.
+    {
+      name: "Motos",
+      image: "/motos.jpg",
+      alt: "Neumáticos para motos en Encarnación",
+      description: "Cubiertas para scooters, naked, deportivas y más",
+      price: "35000",
+    },
+    {
+      name: "Camionetas / Pickups",
+      image: "/pickup.jpg",
+      alt: "Cubiertas para pickups y camionetas en Encarnación",
+      description: "Tracción, carga y durabilidad para todos los caminos",
+      price: "70000",
+    },
   ];
 
   return (
@@ -23,8 +36,6 @@ export const VehicleTypeSection = () => {
       <div 
         className="vehicle-grid"
         itemProp="itemListElement"
-        itemScope
-        itemType="https://schema.org/ListItem"
       >
         {vehicleTypes.map((vehicle, index) => (
           <div 
@@ -44,6 +55,14 @@ export const VehicleTypeSection = () => {
             />
             <h3 itemProp="name">Para {vehicle.name}</h3>
             <p itemProp="description">{vehicle.description}</p>
+
+            {/* Schema.org Offer */}
+            <div itemProp="offers" itemScope itemType="https://schema.org/Offer">
+              <meta itemProp="priceCurrency" content="ARS" />
+              <meta itemProp="price" content={vehicle.price} />
+              <link itemProp="availability" href="https://schema.org/InStock" />
+            </div>
+
             <meta itemProp="position" content={index + 1} />
           </div>
         ))}
